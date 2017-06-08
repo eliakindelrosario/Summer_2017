9efa738cfa9ec1a52f025418ddc407c6593f2ea1
Eliakin del Rosario <eking1566@gmail.com>
2017-06-08 16:22:15 -0400

    Added main.py

---
+++ python-flask-app/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import json 
+
+# Initialize app
+app = Flask(__name__)
+
+# Home route 
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+# About route
+@app.route('/about')
+def about():
+	return render_template('index.html')
+
+# Questionnair route
+# Accepts GET and POST requests
+@app.route('/questionnair', methods=['GET','POST'])
+def questionnair():
+	return render_template('questionnair.html')
+
+# Run this program if called directly
+if __name__ == "__main__":
+	app.secret_key = 'secret'
+	app.run(debug=True)
+	
