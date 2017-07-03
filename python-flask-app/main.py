4cc3a830101110a5fe53468a5823418b356f9f67
Eliakin del Rosario <eking1566@gmail.com>
2017-07-03 13:43:07 -0700

    Removed unwanted features.

--- python-flask-app/main.py
+++ python-flask-app/main.py
@@ -1,36 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
-import json 
+import json
 
 # Initialize app
 app = Flask(__name__)
 
-# Home route 
-@app.route('/')
-def index():
-	return render_template('index.html')
-
-# About route
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
 # Questionnair route
 # Accepts GET and POST requests
-@app.route('/questionnair', methods=['GET','POST'])
-def questionnair():
+@app.route('/', methods=['GET','POST'])
+def questionnaire():
+	""" Render template with form fields."""
+	# check if data was sent
 	if request.method == 'POST':
+		# get the data from the form
 		result = request.form
+		# open json file
 		with open('data.json', 'w+') as json_file:
+			# write data to json file
 			json.dump(result, json_file)
-
-		# return home after form submition 
-		return redirect(url_for('index'))
+		# return complete  message page after form submition
+		return redirect(url_for('complete'))
 	else:
-		# display questionnair by default
-		return render_template('questionnair.html')
+		# display questionnaire by default
+		return render_template('questionnaire.html')
+
+@app.route('/complete')
+def complete():
+	""" Render template with complete message."""
+	return render_template('complete.html')
 
 # Run this program if called directly
 if __name__ == "__main__":
 	app.secret_key = 'secret'
 	app.run(debug=True)
-	
